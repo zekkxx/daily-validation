78ddd65761d52672d467760f6f3c22c24847fe55
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2020-10-06 14:05:03 -0500

    Initial file setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3001;
+require('dotenv').config();
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// app.use(require("./routes"));
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
